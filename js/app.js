e17df56984a2f2b7b793d8f3febcf6b77cbc6269
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-01-28 02:43:43 -0600

    detectar si un producto ya esta en el arreglo

--- js/app.js
+++ js/app.js
@@ -117,5 +117,14 @@ function mostrarPlatillos(platillos){
 }
 
 function agregarPlatillo(producto){
-    console.log(producto);
+    //Extraer el pedido actual
+    let {pedido} = cliente; 
+    
+    //Revisar que la cantidad sea mayor a 0
+    if(producto.cantidad>0){
+        cliente.pedido = [...pedido, producto];
+    } else{
+        console.log("no es mayor a 0");
+    }
+    console.log(cliente.pedido);
 }
