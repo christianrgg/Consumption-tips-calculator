2773a670a16d9c426339380b76e87891e20430d5
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-01-28 01:42:59 -0600

    Mostrando los platillos

--- js/app.js
+++ js/app.js
@@ -56,6 +56,23 @@ function obtenerPlatillos(){
     const url = `http://localhost:4000/platillos`;
     fetch(url) 
         .then(respuesta => respuesta.json())
-        .then(resultado => console.log(resultado))
+        .then(resultado => mostrarPlatillos(resultado))
         .catch(error => console.log(error))
+}
+
+function mostrarPlatillos(platillos){
+    // console.log(platillos);
+    const contenido = document.querySelector(`#platillos .contenido`);
+    platillos.forEach(platillo =>{
+        const row = document.createElement(`div`);
+        row.classList.add(`row`);
+
+        const nombre = document.createElement(`div`);
+        nombre.classList.add(`col-md-4`);
+        nombre.textContent = platillo.nombre;
+
+        row.appendChild(nombre);
+        contenido.appendChild(row);
+    })
+
 }
