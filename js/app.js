2a5070dea8e497fe7695b1654fd0b9ed81eaf633
Christian Roberto <90073226+christianrgg@users.noreply.github.com>
2023-01-27 21:23:49 -0600

    Validando formulario y mostrando alerta

--- js/app.js
+++ js/app.js
@@ -14,8 +14,23 @@ function guardarCliente(){
     //Revisar si hay campos vacios
     const camposVacios = [mesa, hora].some(campo => campo === ``);
     if(camposVacios){
-        console.log(`Si hay un campo vacio`);
-    } else{
-        console.log("Todos los campos estan llenos");
-    }
+        //Verificar si existe alerta
+        const existeAlerta = document.querySelector(`.invalid-feedback`);
+        if (!existeAlerta){
+            const alerta = document.createElement(`div`);
+            alerta.classList.add(`invalid-feedback`, `d-block`, `text-center`);
+            alerta.textContent = `Todos los campos son obligatorios`;
+            document.querySelector(`.modal-body form`).appendChild(alerta);
+
+            //Eliminar alerta
+            setTimeout(()=>{
+                alerta.remove();
+            },3000)
+        }  
+
+        return
+    } 
+        
+    console.log("Todos los campos estan llenos");
+
 }
